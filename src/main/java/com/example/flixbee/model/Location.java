611708d24c63d8b99aa20455fcf3ccda9f5fb4b8
Germain Mathis <mgermain@ca-cf.fr>
2022-06-22 12:28:03 +0200

    add location model

---
+++ src/main/java/com/example/flixbee/model/Location.java
@@ -0,0 +1,59 @@
+package com.example.flixbee.model;
+
+public class Location {
+
+    private String name;
+    private String city;
+    private String street;
+    private String numero;
+    private String phoneNumber;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
